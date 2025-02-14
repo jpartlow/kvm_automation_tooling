564a3bc63c22125f03b77f48aa794098335539fe
Josh Partlow <joshua.partlow@puppetlabs.com>
2025-02-14 15:27:10 -0800

    Fix up main.tf
    * Use a current image
    * Clean up some of the pathing
    * Use network bridge
    * Use cloud-init meta-data

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,42 +1,51 @@
-#https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.8.1/examples/v0.13/ubuntu/ubuntu-example.tf
+# Originally from: https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.8.1/examples/v0.13/ubuntu/ubuntu-example.tf
 terraform {
- required_version = ">= 0.13"
   required_providers {
     libvirt = {
-      source  = "dmacvicar/libvirt"
-      version = "0.6.2"
+      source = "dmacvicar/libvirt"
     }
   }
 }
 
-# instance the provider
+locals {
+  # The path to the cloud-init configuration file
+  cloud_init_path = "${path.module}/../cloud-init/ubuntu-2404-amd64"
+}
+
 provider "libvirt" {
   uri = "qemu:///system"
 }
 
 resource "libvirt_pool" "ubuntu" {
-  name = "ubuntu"
-  type = "dir"
-  path = "/tmp/terraform-provider-libvirt-pool-ubuntu"
+  name   = "ubuntu"
+  type   = "dir"
+  target {
+    path = "/var/lib/libvirt/images/ubuntu-2404-amd64"
+  }
 }
 
 # We fetch the latest ubuntu release image from their mirrors
 resource "libvirt_volume" "ubuntu-qcow2" {
-  name   = "ubuntu-qcow2"
+  name = "ubuntu-qcow2"
+#  pool = "default"
   pool   = libvirt_pool.ubuntu.name
-  source = "https://cloud-images.ubuntu.com/releases/xenial/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
+  source = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
   format = "qcow2"
 }
 
 data "template_file" "user_data" {
-  template = file("${path.module}/cloud_init.cfg")
+  template = file("${local.cloud_init_path}/user-data.yaml")
 }
 
 data "template_file" "network_config" {
-  template = file("${path.module}/network_config.cfg")
+  template = file("${local.cloud_init_path}/network-config.yaml")
 }
 
-# for more info about paramater check this out
+data "template_file" "meta_data" {
+  template = file("${local.cloud_init_path}/meta-data.yaml")
+}
+
+# for more info about parameters check this out
 # https://github.com/dmacvicar/terraform-provider-libvirt/blob/master/website/docs/r/cloudinit.html.markdown
 # Use CloudInit to add our ssh-key to the instance
 # you can add also meta_data field
@@ -44,19 +53,22 @@ resource "libvirt_cloudinit_disk" "commoninit" {
   name           = "commoninit.iso"
   user_data      = data.template_file.user_data.rendered
   network_config = data.template_file.network_config.rendered
+  meta_data      = data.template_file.meta_data.rendered
   pool           = libvirt_pool.ubuntu.name
 }
 
 # Create the machine
 resource "libvirt_domain" "domain-ubuntu" {
-  name   = "ubuntu-terraform"
-  memory = "512"
-  vcpu   = 1
+  name   = "ubuntu-terraform-2"
+  memory = "2048"
+  vcpu   = 2
 
   cloudinit = libvirt_cloudinit_disk.commoninit.id
 
   network_interface {
-    network_name = "default"
+     bridge = "virbr0"
+     # Should test with this enabled
+     # wait_for_lease = true
   }
 
   # IMPORTANT: this is a known bug on cloud images, since they expect a console
@@ -78,11 +90,9 @@ resource "libvirt_domain" "domain-ubuntu" {
     volume_id = libvirt_volume.ubuntu-qcow2.id
   }
 
-  graphics {
-    type        = "spice"
-    listen_type = "address"
-    autoport    = true
-  }
+  #  graphics {
+  #    type        = "spice"
+  #    listen_type = "address"
+  #    autoport    = true
+  #  }
 }
-
-# IPs: use wait_for_lease true or after creation use terraform refresh and terraform show for the ips of domain
