3b15bb6ae6d4cca901338afe00384e7e06110170
Josh Partlow <joshua.partlow@puppetlabs.com>
2025-04-09 16:26:36 -0700

    (plans) Add a subplan to lookup target platform from facts
    Used to set a platform variable on each target based on the target
    facts. Targets without facter (before puppet is installed) return a
    basic set of os facts which does not include architecture. The plan
    turns to `uname -m` for that case.
    
    This will be used to obtain platform strings for beaker host files for
    running acceptance tests, for example.

---
+++ spec/plans/subplans/lookup_platform_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+
+describe 'plan: kvm_automation_tooling::subplans::lookup_platform' do
+  include_context 'plan_init'
+
+  let(:facts) do
+    {
+      'os' => {
+        'name' => 'Ubuntu',
+        'release' => {
+          'full' => '24.04',
+          'major' => '24',
+          'minor' => '04',
+        },
+      },
+    }
+  end
+
+  def inventory_data
+    {
+      'facts' => facts,
+      'targets' => [
+        'spec',
+      ],
+    }
+  end
+
+  before(:each) do
+    expect_task('facts').with_targets('spec').always_return({})
+  end
+
+  it 'returns a target with platform var set' do
+    expect_command('uname -m').with_targets('spec').always_return('stdout' => "x86_64\n")
+
+    result = run_plan('kvm_automation_tooling::subplans::lookup_platform', 'targets' => 'spec')
+
+    expect(result.ok?).to(eq(true), result.value.to_s)
+    expect(result.value.first.vars).to include(
+      'platform' => 'ubuntu-2404-amd64',
+    )
+  end
+
+  context 'when facter is present' do
+    let(:facts) do
+      {
+        'os' => {
+          'name' => 'Ubuntu',
+          'release' => {
+            'full' => '24.04',
+            'major' => '24',
+            'minor' => '04',
+          },
+          'architecture' => 'x86_64',
+        },
+      }
+    end
+
+    it 'returns a target with platform var set without a separate uname lookup' do
+      result = run_plan('kvm_automation_tooling::subplans::lookup_platform', 'targets' => 'spec')
+
+      expect(result.ok?).to(eq(true), result.value.to_s)
+      expect(result.value.first.vars).to include(
+        'platform' => 'ubuntu-2404-amd64',
+      )
+    end
+  end
+end
