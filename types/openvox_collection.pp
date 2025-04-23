37db7ef1c47108623b433213ce5579716ebcca47
Josh Partlow <joshua.partlow@puppetlabs.com>
2025-04-23 16:07:15 -0700

    (plans) Add some puppet types to help manage install parameters
    for installing openvox-agent. This will allow passing a hash of params
    for openvox installation to standup_cluster which can hand them off to
    the install_openvox subplan.

---
+++ types/openvox_collection.pp
@@ -0,0 +1,6 @@
+# Strings identifying an openvox collection (packages associated with
+# a major version of OpenVox).
+type Kvm_automation_tooling::Openvox_collection = Enum[
+  'openvox7',
+  'openvox8',
+]
