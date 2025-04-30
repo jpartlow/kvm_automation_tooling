dc24b67a086cb5176ebb9d3cac9815f69d894371
Josh Partlow <joshua.partlow@puppetlabs.com>
2025-04-30 10:56:42 -0700

    (os) Add debian-13 as an allowed platform.

--- types/allowed_platforms.pp
+++ types/allowed_platforms.pp
@@ -3,6 +3,7 @@ type Kvm_automation_tooling::Allowed_platforms = Enum[
   'debian-10-amd64',
   'debian-11-amd64',
   'debian-12-amd64',
+  'debian-13-amd64',
   'ubuntu-1804-amd64',
   'ubuntu-2004-amd64',
   'ubuntu-2204-amd64',
