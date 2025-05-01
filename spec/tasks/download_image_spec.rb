f63dea8880012834846d75ad4d88aad59e016886
Josh Partlow <joshua.partlow@puppetlabs.com>
2025-04-30 23:58:36 -0700

    (tasks) Fail download_image task for 400+ http responses
    Curl without --fail or --fail-with-body stores the http response for 400
    response like 404 not found in the -o file and exits 0. This would lead
    to a silent failure where the pipeline would later grind to a halt after
    libvirt attempts to stand up an image consisting of the http doc for a
    404 response, and then terraform complains that their is no
    networking...
    
    Anyway, adding --fail-with-body captures the response and exits
    non-zero, and the task reads that, clears the file and raises an error
    with the curl output and the http response.
    
    Sadly this doesn't necessarily print nicely wrapped in Bolt's parallelize(),
    but it does fail, and the output does read nicely with --verbose. But
    with --stream the PlanResult gets dumped as unformatted json.

--- spec/tasks/download_image_spec.rb
+++ spec/tasks/download_image_spec.rb
@@ -13,33 +13,86 @@ describe 'task: download_image.rb' do
     end
 
     it 'downloads the image if not present' do
-      expect(Open3).to receive('capture2e').with('curl', '-L', '-o', 'DIR/IMAGE', 'URL/IMAGE').and_return(['output', success])
-  
-      expect(task.task(image_url: 'URL/IMAGE', download_dir: 'DIR')).to match(
+      expect(Open3).to receive('capture2e').with('curl', '--fail-with-body', '-L', '-o', '/DIR/IMAGE', 'URL/IMAGE').and_return(['output', success])
+
+      expect(task.task(image_url: 'URL/IMAGE', download_dir: '/DIR')).to match(
         {
-          path: 'DIR/IMAGE',
+          curl: ['curl', '--fail-with-body', '-L', '-o', '/DIR/IMAGE', 'URL/IMAGE'],
+          output: 'output',
+          path: '/DIR/IMAGE',
           already_exists: false,
         }
       )
     end
 
     it 'does nothing if image is present' do
-      expect(File).to receive('exist?').with('DIR/IMAGE').and_return(true)
-      expect(task.task(image_url: 'URL/IMAGE', download_dir: 'DIR')).to match(
+      expect(File).to receive('exist?').with('/DIR/IMAGE').and_return(true)
+      expect(task.task(image_url: 'URL/IMAGE', download_dir: '/DIR')).to match(
         {
-          path: 'DIR/IMAGE',
+          path: '/DIR/IMAGE',
           already_exists: true,
         }
       )
     end
 
-    # ~/images doesn't get expanded within an Open3 call...
-    it 'expands download_dir to an absolute path'
+    it 'expands download_dir to an absolute path' do
+      expanded_dir = "#{ENV['HOME']}/DIR"
+      expect(Open3).to receive('capture2e').with('curl', '--fail-with-body', '-L', '-o', "#{expanded_dir}/IMAGE", 'URL/IMAGE').and_return(['output', success])
+
+      expect(task.task(image_url: 'URL/IMAGE', download_dir: '~/DIR')).to match(
+        {
+          curl: ['curl', '--fail-with-body', '-L', '-o', "#{expanded_dir}/IMAGE", 'URL/IMAGE'],
+          output: 'output',
+          path: "#{expanded_dir}/IMAGE",
+          already_exists: false,
+        }
+      )
+    end
+
+    it 'raises an error if curl fails' do
+      expect(Open3).to receive('capture2e').with('curl', '--fail-with-body', '-L', '-o', '/DIR/IMAGE', 'URL/IMAGE').and_return(['output', failure])
+      expect do
+        task.task(image_url: 'URL/IMAGE', download_dir: '/DIR')
+      end.to(
+        raise_error(TaskHelper::Error, %r{Failed to download 'URL/IMAGE' to '/DIR/IMAGE'})
+      )
+    end
+
+    context 'with http response' do
+      let(:tmpdir) { Dir.mktmpdir('kat-download_image_spec') }
+      let(:output_path) { File.join(tmpdir, 'IMAGE') }
+
+      around(:each) do |example|
+        example.run
+      ensure
+        FileUtils.remove_entry_secure(tmpdir)
+      end
+
+      it 'raises an error if curl fails with http response' do
+        expect(Open3).to(
+          # --fail-with-body causes curl to exit non-zero for HTTP
+          # 400+ responses, and save the response body to the file...
+          receive('capture2e').with('curl', '--fail-with-body', '-L', '-o', output_path, 'URL/IMAGE') do
+            File.write(output_path, 'HTTP 404 not found')
+            ['output', failure]
+          end
+        )
+
+        expect do
+         task.task(image_url: 'URL/IMAGE', download_dir: tmpdir)
+        end.to(
+          raise_error(
+            TaskHelper::Error,
+            %r{Failed to download 'URL/IMAGE' to '#{output_path}'.+Http response:.+HTTP 404 not found}m
+          )
+        )
+      end
+    end
   end
 
   it 'raises an error if curl not found' do
     expect(Open3).to receive('capture3').with('which', 'curl').and_return(['', '', failure])
-    expect { task.task(image_url: 'URL/IMAGE', download_dir: 'DIR') }.to raise_error(TaskHelper::Error, /Command 'curl' not found/)
+    expect { task.task(image_url: 'URL/IMAGE', download_dir: '/DIR') }.to raise_error(TaskHelper::Error, /Command 'curl' not found/)
   end
 end
 
