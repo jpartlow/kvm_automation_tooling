3876d749add28bc4df345ce50b2600ffd1c82da5
Josh Partlow <joshua.partlow@puppetlabs.com>
2025-04-30 20:47:58 -0700

    (os) Add a function to get normalized arch value for os
    Ensures amd64/arm64 for debian/ubuntu, x86_64/aarch64 otherwise.

---
+++ functions/get_normalized_os_arch.pp
@@ -0,0 +1,23 @@
+# Return the expected architecture strings for the given os. (amd64,
+# arm64 for debian/ubuntu, and x86_64 aarch64 for others)
+function kvm_automation_tooling::get_normalized_os_arch(
+  Kvm_automation_tooling::Operating_system $os,
+  Kvm_automation_tooling::Os_arch $arch,
+) {
+  case $os {
+    'debian','ubuntu': {
+      $arch ? {
+        'x86_64'  => 'amd64',
+        'aarch64' => 'arm64',
+        default   => $arch,
+      }
+    }
+    default: {
+      $arch ? {
+        'amd64' => 'x86_64',
+        'arm64' => 'aarch64',
+        default => $arch,
+      }
+    }
+  }
+}
